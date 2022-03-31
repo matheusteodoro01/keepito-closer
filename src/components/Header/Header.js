ec201765435a2847b43970b572f8a14772604c65
Matheus Teodoro <matheusteodoro01@hotmail.com>
2022-03-31 11:44:45 -0300

    fix: header name

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -137,7 +137,7 @@ export default function Header(props) {
           )}
         </IconButton>
         <Typography variant="h6" weight="medium" className={classes.logotype}>
-          React Material Admin
+          Keepito
         </Typography>
         <div className={classes.grow} />
         <div
