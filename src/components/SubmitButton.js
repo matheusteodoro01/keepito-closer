7a907921ca5ad28246e845ef9da523daad13414c
unknown <rossatto.gomes30@gmail.com>
2022-05-04 21:07:16 -0300

    fix: update design of gforms

---
+++ src/components/SubmitButton.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+import DoneIcon from "@material-ui/icons/Done";
+
+export default function SubmitButton(props) {
+    return (
+        <Button
+            onClick={props.subimit}
+            variant="outlined"
+            color="primary"
+            endIcon={<DoneIcon />}>
+            Submit
+        </Button>
+    );
+}
