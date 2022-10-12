39381c3f020653e899dfdd3fcd7103526e914418
Matheus Teodoro <matheus.teodoro@farmaciasapp.com.br>
2022-10-12 19:07:21 -0300

    feat: add new modal SaveAlternative style

--- src/components/styles.js
+++ src/components/styles.js
@@ -1,69 +1,80 @@
 import { makeStyles } from "@material-ui/styles";
 
-export default makeStyles(theme => ({
-    boxModalCourseForm: {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: 1150,
-        background: '#FFFFFF',
-        'border-radius': '25px',
-        padding: 5,
-        'max-height': '95%',
-        overflow: "hidden",
-        overflowY: "scroll", // added scroll
-        'background-color': 'rgba(255,255,255,0.87)'
-    },
-    boxModalClassesForm: {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: 500,
-        background: '#FFFFFF',
-        'border-radius': '25px',
-        padding: 5,
-        'background-color': 'rgba(255,255,255,0.87)',
-        'max-height': '95%',
-        overflow: "hidden",
-        overflowY: "scroll" // added scroll
-    },
+export default makeStyles((theme) => ({
+  boxModalCourseForm: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 1150,
+    background: "#FFFFFF",
+    "border-radius": "25px",
+    padding: 5,
+    "max-height": "95%",
+    overflow: "hidden",
+    overflowY: "scroll", // added scroll
+    "background-color": "rgba(255,255,255,0.87)",
+  },
+  boxModalClassesForm: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 500,
+    background: "#FFFFFF",
+    "border-radius": "25px",
+    padding: 5,
+    "background-color": "rgba(255,255,255,0.87)",
+    "max-height": "95%",
+    overflow: "hidden",
+    overflowY: "scroll", // added scroll
+  },
 
-    boxModalCreateQuizForm: {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        minWidth: '50%',
-        maxWidth: '70%',
-        background: '#FFFFFF',
-        'border-radius': '25px',
-        padding: 6,
-        'background-color': 'rgba(255,255,255,0.87)',
-        'max-height': '90%',
-        overflow: "hidden",
+  boxModalCreateQuizForm: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    minWidth: "50%",
+    maxWidth: "70%",
+    background: "#FFFFFF",
+    "border-radius": "25px",
+    padding: 6,
+    "background-color": "rgba(255,255,255,0.87)",
+    "max-height": "90%",
+    overflow: "hidden",
+    overflowY: "scroll",
+  },
 
-    },
-    form: {
-        background: '#FFFFFF',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: theme.spacing(4),
+  boxModalSaveAlternativeForm: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    minWidth: "50%",
+    maxWidth: "70%",
+    background: "#FFFFFF",
+    "border-radius": "25px",
+    padding: 6,
+    "background-color": "rgba(255,255,255,0.87)",
+    "max-height": "90%",
+    overflow: "hidden",
+  },
+  form: {
+    background: "#FFFFFF",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: theme.spacing(4),
 
-        '& .MuiTextField-root': {
-            margin: theme.spacing(1),
-            width: '100%',
-        },
-        '& .MuiButtonBase-root': {
-            margin: theme.spacing(1),
-        },
-        '& .MuiGrid-root': {
-            margin: theme.spacing(1),
-            width: '100%',
-        },
+    "& .MuiButtonBase-root": {
+      margin: theme.spacing(1),
     },
+    "& .MuiGrid-root": {
+      margin: theme.spacing(1),
+      width: "100%",
+    },
+    scroll: "true",
+  },
 }));
-
