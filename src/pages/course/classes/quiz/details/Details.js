489f768fafa2279f42c4d18d04881904ab542218
Matheus Teodoro <matheus.teodoro@farmaciasapp.com.br>
2022-10-19 17:11:40 -0300

    feat:  add styles on quiz details

--- src/pages/course/classes/quiz/details/Details.js
+++ src/pages/course/classes/quiz/details/Details.js
@@ -117,14 +117,7 @@ export default function DetailsQuiz(props) {
               Adicionar Questão
             </Button>
 
-            <CardActions disableSpacing>
-              <IconButton aria-label="add to favorites">
-                <FavoriteIcon />
-              </IconButton>
-              <IconButton aria-label="share">
-                <ShareIcon />
-              </IconButton>
-            </CardActions>
+
           </CardContent>
         </Grid>
       </Grid>
