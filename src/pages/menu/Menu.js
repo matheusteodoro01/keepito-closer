d3d7d046b0805f0d42e1a5a91917eb64e8819434
Matheus Teodoro <matheusteodoro01@hotmail.com>
2022-10-20 21:41:21 -0300

    feat: add button add course

--- src/pages/menu/Menu.js
+++ src/pages/menu/Menu.js
@@ -9,6 +9,8 @@ import {
 } from "@material-ui/core";
 import Stack from "@mui/material/Stack";
 import Card from "@material-ui/core/Card";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
 
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
@@ -43,65 +45,79 @@ export default function Menu() {
 
   return (
     <>
-      <Modal open={showModal} onClose={() => setShowModal(false)}>
-        <Box className={classesNames.boxModalCreateQuizForm}>
-          <SaveCourse
-            userId={userId}
-            setShowModal={setShowModal}
-            courseSelected={courseSelected}
-            courses={courses}
-            setCourses={setCourses}
-          />
-        </Box>
-      </Modal>
-      <Grid container spacing={2}>
-        {courses?.map((course) => (
-          <Grid item xs={12} sm={4} key={course.id}>
-            <Card
-              style={{
-                maxWidth: 345,
-                maxHeight: 345,
-                boxShadow: "0 5px 8px 0 rgba(0, 0, 0, 0.3)",
-                backgroundColor: "#fafafa",
-              }}
-            >
-              <CardActionArea
-                component={RouterLink}
-                to={`/app/course/details/${course.id}`}
-              ></CardActionArea>
-              <CardMedia
-                component="img"
-                height="200"
-                image="https://escuelafullstack.com/web/image/slide.channel/18/image_512"
-                alt="green iguana"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h2" component="div">
-                  {course.name}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {course.description}
-                </Typography>
-                <Stack
-                  direction="column"
-                  alignItems="flex-end"
-                  width="100%"
-                  spacing={10}
-                >
-                  <IconButton
-                    size="small"
-                    onClick={() => {
-                      setCourseSelect(course);
-                      setShowModal(true);
-                    }}
+      <Grid container direction="column" alignItems="flex-end" m={2}>
+        <Modal open={showModal} onClose={() => setShowModal(false)}>
+          <Box className={classesNames.boxModalCreateQuizForm}>
+            <SaveCourse
+              userId={userId}
+              setShowModal={setShowModal}
+              courseSelected={courseSelected}
+              courses={courses}
+              setCourses={setCourses}
+            />
+          </Box>
+        </Modal>
+        <Button
+        style={{margin:10}}
+          variant="contained"
+          color="success"
+          onClick={() => setShowModal(true)}
+        >
+          Criar Curso
+        </Button>
+        <Grid container spacing={2}>
+          {courses?.map((course) => (
+            <Grid item xs={12} sm={4} key={course.id}>
+              <Card
+                style={{
+                  maxWidth: 345,
+                  maxHeight: 345,
+                  boxShadow: "0 5px 8px 0 rgba(0, 0, 0, 0.3)",
+                  backgroundColor: "#fafafa",
+                }}
+              >
+                <CardActionArea
+                  component={RouterLink}
+                  to={`/app/course/details/${course.id}`}
+                ></CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="200"
+                  image="https://escuelafullstack.com/web/image/slide.channel/18/image_512"
+                  alt="green iguana"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h2" component="div">
+                    {course.name}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {course.description}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {course?.classes.length}{" "}
+                    {course?.classes.length === 1 ? "aula" : "aulas"}{" "}
+                  </Typography>
+                  <Stack
+                    direction="column"
+                    alignItems="flex-end"
+                    width="100%"
+                    spacing={10}
                   >
-                    <EditIcon />
-                  </IconButton>
-                </Stack>
-              </CardContent>
-            </Card>
-          </Grid>
-        ))}
+                    <IconButton
+                      size="small"
+                      onClick={() => {
+                        setCourseSelect(course);
+                        setShowModal(true);
+                      }}
+                    >
+                      <EditIcon />
+                    </IconButton>
+                  </Stack>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </Grid>
     </>
   );
